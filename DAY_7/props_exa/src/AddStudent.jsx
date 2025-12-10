2306ed7150a810ea4c8ecc419f4302bbfbfd5016
Sreemathi Manoharan <sreemathi2106@gmail.com>
2025-12-10 15:50:58 +0530

    Day 7 updated AddStudent.jsx added new hook function 'useRef()'

--- DAY_7/props_exa/src/AddStudent.jsx
+++ DAY_7/props_exa/src/AddStudent.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
-
+import { useRef } from 'react'
+//uisng useRef hook to focus the input field after submitting the form(control moves from submit button to input field after clicking submit button)
 const AddStudent = ({newstu,setNewStu,handleSubmit}) => {
+  const inputRef=useRef();
+
   return (
     <div>
         <form action="" onSubmit={handleSubmit}>
             <input type="text" placeholder='New Student'
+                               ref={inputRef}
                                value={newstu}
                                onChange={(e)=>setNewStu(e.target.value)}
+
             />
-            <button type='submit'>Submit</button>
+
+            <button type='submit' onClick={()=>inputRef.current.focus()}>Submit</button>
         </form>
     </div>
   )
